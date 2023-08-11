4d4ea3b112b777b01a6d2372709cdb8e87506b9e
liyang <2234333815@qq.com>
2023-08-11 16:12:56 +0800

    2023/8/11 完成数据可视化Web应用

---
+++ app.py
@@ -0,0 +1,60 @@
+import sqlite3
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/index')
+def home():
+    # return render_template('index.html')
+    return index()
+
+
+@app.route('/movie')
+def movie():
+    datalist = []
+    con = sqlite3.connect('movieTop250.db')
+    cur = con.cursor()
+    sql = 'select * from movie250'
+    data = cur.execute(sql)
+    for item in data:
+        datalist.append(item)
+    cur.close()
+    con.close()
+    return render_template('movie.html', movies=datalist)
+
+
+@app.route('/score')
+def score():
+    score = []  # 评分
+    count = []  # 每个评分的电影数量
+    con = sqlite3.connect('movieTop250.db')
+    cur = con.cursor()
+    sql = 'select score,count(score) from movie250 group by score'
+    data = cur.execute(sql)
+    for item in data:
+        score.append(str(item[0]) + '分')
+        count.append(item[1])
+    cur.close()
+    con.close()
+    return render_template('score.html', score=score, count=count)
+
+
+@app.route('/team')
+def team():
+    return render_template('team.html')
+
+
+@app.route('/word')
+def word():
+    return render_template('word.html')
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ wordCloud.py
@@ -0,0 +1,48 @@
+# coding=utf-8
+# @Time: 2023/8/11 15:29
+# @Author: liyang
+# @File: wordCloud.py
+# @Software: PyCharm
+
+import jieba
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+import numpy as np
+from PIL import Image
+import sqlite3
+
+# 1.准备词云所需的文字
+con = sqlite3.connect('movieTop250.db')
+cur = con.cursor()
+sql = 'select introduction from movie250'
+data = cur.execute(sql)
+text = ""
+for item in data:
+    text = text + item[0]
+cur.close()
+con.close()
+
+# 2.分词
+cut = jieba.cut(text)
+string = ' '.join(cut)
+print(len(string))
+
+# 3.创建遮罩
+img = Image.open(r'.\static\assets\img\tree.jpg')  # 打开遮罩图片
+img_array = np.array(img)  # 将图片转换为数组
+wc = WordCloud(
+    background_color='white',
+    mask=img_array,
+    font_path="msyh.ttc"
+)
+wc.generate_from_text(string)
+
+# 4.绘制图片
+fig = plt.figure(1)
+plt.imshow(wc)
+plt.axis('off')  # 是否显示坐标轴
+
+# plt.show()  # 显示生成的词云图片
+# 5.输出词云图片到文件
+plt.savefig(r'.\static\assets\img\wordCloud.jpg', dpi=200)
+
